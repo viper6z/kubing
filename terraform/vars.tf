28008c7104482d2ce2f4131e0cca86ffc2d0aca6
viper6z <oskarbhc@gmail.com>
2026-07-06 23:30:15 +0200

    add vars

---
+++ terraform/vars.tf
@@ -0,0 +1,10 @@
+variable "admin_cidr" {
+  type = string
+  description = "cidr allowed to ssh into nodes"
+
+}
+
+variable "availability_zone" {
+  type = string
+  description = "availability zone for subnet"
+}
